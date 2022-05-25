e8e3212d388290aef538af4510a3681dcbcd7ad8
Gabriele Cotigliani <65671410+gabrielmax1@users.noreply.github.com>
2022-05-25 10:24:51 +0100

    Add files via upload

---
+++ Code/References.py
@@ -0,0 +1,22 @@
+'''
+There are the following sources that I used for my research, I tried all of the following approaches
+testing they're results and studying they're approach to the FER solution
+Looking in particular at Data Augmentation, where it was implemented, models structure, regularizations techniques
+(e.g., L2, Dropout) and so on.
+
+References:
+repository: https://github.com/gitshanks/fer2013
+Author: Nishank Sharma
+
+repository: https://github.com/Tanoy004/Emotion-recognition-through-CNN
+Authors: Tanoy Debnath, Mahfuz Reza, Anichur Rahman, Shahab S. Band
+
+repository: https://github.com/bnsreenu/python_for_microscopists/blob/master/239_train_emotion_detection/239_train_emotion_detection.py
+Author: Dr. Sreenivas Bhattiprolu
+
+repository: https://www.kaggle.com/shawon10/facial-expression-detection-cnn
+Author: ASHADULLAH SHAWON
+
+repository: https://github.com/Aaditya1978/Face_Expression_Prediction
+Author: Aaditya
+'''
